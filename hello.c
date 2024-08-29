acb40719a297f77c2c2985ee2254313ca3c56522
Keenan Flannery <kflannery2@huskers.unl.edu>
2024-08-29 11:14:24 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Keenan Flannery
+ *         kflannery2@huskers.unl.edu
+ * Date: 2024/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Keenan Flannery\n");
+
+  return 0;
+}
